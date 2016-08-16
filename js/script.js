7a804fa06bff5857524e2c43f1863a8169f46b73
Rush3D <vanesdemon96@gmail.com>
2016-08-16 20:52:40 +0700

    3-е задание

---
+++ js/script.js
@@ -0,0 +1,14 @@
+var radio_1 = document.getElementById("radio-1");
+radio_1.onchange = function() {
+  document.body.style.backgroundColor = "#849d8f";
+};
+
+var radio_2 = document.getElementById("radio-2");
+radio_2.onchange = function() {
+  document.body.style.backgroundColor = "#8996a6";
+};
+
+var radio_3 = document.getElementById("radio-3");
+radio_3.onchange = function() {
+  document.body.style.backgroundColor = "#9d8b84";
+};                 
